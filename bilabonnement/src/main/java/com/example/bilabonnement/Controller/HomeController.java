f1b8b1b27ec8b7c34254be1ff102f8b190e2736c
PigeonPC <olgakatinka@gmail.com>
2025-12-01 11:44:03 +0100

    changes

---
+++ bilabonnement/src/main/java/com/example/bilabonnement/Controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.bilabonnement.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return"home/index";
+    }
+
+    @PostMapping("/")
+    public String handlePost() {
+        //noget med at håndtere formmular eller lign.
+        return "home/index"; //eller noget...
+    }
+
+}
