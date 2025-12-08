8da0b27e126b5d9905f4b89d605d40b1038c1068
SGyldenkvist <silke@gyldenkvist.dk>
2025-12-08 09:04:49 +0100

    CarView, CarService, CarRepository + Controllere + html: carDetail, cars, carsByStatus

---
+++ bilabonnement/src/main/java/com/example/bilabonnement/dataRegistration/model/CarView.java
@@ -0,0 +1,132 @@
+package com.example.bilabonnement.dataRegistration.model;
+
+public class CarView {
+
+    private Integer vehicleId;
+    private String chassisNumber;
+    private String brand;
+    private String model;
+    private String equipmentLevel;
+    private Double steelPrice;
+    private Double registrationTax;
+    private Integer co2Emission;
+    private Integer mileage;
+    private String leasingCode;
+    private String irkCode;
+    private String dateOfPurchase;
+    private Double purchasePrice;
+
+    private String carStatus;
+
+    public Integer getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Integer vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getChassisNumber() {
+        return chassisNumber;
+    }
+
+    public void setChassisNumber(String chassisNumber) {
+        this.chassisNumber = chassisNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getEquipmentLevel() {
+        return equipmentLevel;
+    }
+
+    public void setEquipmentLevel(String equipmentLevel) {
+        this.equipmentLevel = equipmentLevel;
+    }
+
+    public Double getSteelPrice() {
+        return steelPrice;
+    }
+
+    public void setSteelPrice(Double steelPrice) {
+        this.steelPrice = steelPrice;
+    }
+
+    public Double getRegistrationTax() {
+        return registrationTax;
+    }
+
+    public void setRegistrationTax(Double registrationTax) {
+        this.registrationTax = registrationTax;
+    }
+
+    public Integer getCo2Emission() {
+        return co2Emission;
+    }
+
+    public void setCo2Emission(Integer co2Emission) {
+        this.co2Emission = co2Emission;
+    }
+
+    public Integer getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(Integer mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getLeasingCode() {
+        return leasingCode;
+    }
+
+    public void setLeasingCode(String leasingCode) {
+        this.leasingCode = leasingCode;
+    }
+
+    public String getIrkCode() {
+        return irkCode;
+    }
+
+    public void setIrkCode(String irkCode) {
+        this.irkCode = irkCode;
+    }
+
+    public String getDateOfPurchase() {
+        return dateOfPurchase;
+    }
+
+    public void setDateOfPurchase(String dateOfPurchase) {
+        this.dateOfPurchase = dateOfPurchase;
+    }
+
+    public Double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(Double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public String getCarStatus() {
+        return carStatus;
+    }
+
+    public void setCarStatus(String carStatus) {
+        this.carStatus = carStatus;
+    }
+}
